849b2b9e7014521bfcd00bf0a54c78036f2b7c93
AJHJ <41326089+AJHJ@users.noreply.github.com>
2026-06-26 22:26:59 -0600

    adding support for hosting

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 1. Array of specific allowed hostnames
+    allowedHosts: [
+      'mydogs.onrender.com',
+    ],
+  }
 })
