90378f514e981ef0277ef87b934144200c45d1d8
AJHJ <41326089+AJHJ@users.noreply.github.com>
2026-07-02 22:56:25 -0600

    Must add a way to manage images on gallery

--- my-react-app/src/GalleryCard.jsx
+++ my-react-app/src/GalleryCard.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 function GalleryCard(props){
+    const [comp, setComp] = useState(<></>);
+
+    useEffect(() => {
+        if(props.imageURL.endsWith("mp4")){
+            setComp(<video controls>
+                <source src={"/images/"+props.imageURL} type="video/mp4" />
+                Your browser does not support the video tag.
+                </video>);
+        }else{
+        setComp(<img src={"/images/"+props.imageURL} alt="Not Found"></img>);
+        }
+
+    }, []);
 
     return(
         <div className="card">
-            <img src={"/images/"+props.imageURL} alt="Not Found"></img>
+            {comp}
 
         </div>
     );
