9a3cb4d01af33eb294099906270ca9c5b50436d0
Aaron <aaronhdez9123@gmail.com>
2026-07-29 00:20:55 -0600

    Must fix error

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { PageContext } from "./Contexts.jsx";
 
 function Header(){
@@ -7,45 +7,53 @@ function Header(){
     const {username, setUsername} = useContext(PageContext);
 
     //Default values are for logging in
-    const logInOut = () => setBodyContent("Login");
-    const logInOutStr = "Login";
+    const [logInOut, setLogInOut] = useState(() => {setBodyContent("Login")});
+    const [logInOutStr, setLogInOutStr] = useState("Login");
+    const [userMenuOptions, setUserMenuOptions] = useState(<></>);
 
-    //If the username is not guest then set the button text as logout and set the callback function for loggin out
-    if(username != "Guest"){
 
-        logInOutStr = "Logout";
+    useEffect(() => {
 
-        logInOut = async () => {
+        //If the username is not guest then set the button text as logout and set the callback function for loggin out
+        if(username != "Guest"){
 
-        try {
-            const response = await fetch("/logout", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({}),
-            });
+            setLogInOutStr("Logout");
 
-            const result = await response.json();
+            setLogInOut(async () => {
 
-            if (!response.ok) {
-                throw new Error(result.message || "Something went wrong.");
-            }
+                try {
+                    const response = await fetch("/logout", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({}),
+                    });
 
-            if(result.isSuccess == true){
-                logInOut = () => setBodyContent("Login");
-                logInOutStr = "Login";
-                setUsername("Guest");
-            }else{
-                setLoginResult('Error: Something went wrong.');
-            }
+                const result = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(result.message || "Something went wrong.");
+                }
+
+                if(result.isSuccess == true){
+                    setLogInOut(() => setBodyContent("Login"));
+                    setLogInOutStr("Login");
+                    setUsername("Guest");
+                    setUserMenuOptions(<></>);
+                }else{
+                    setLoginResult('Error: Something went wrong.');
+                }
 
       
-        } catch (error) {
+                } catch (error) {
 
-            console.log(error.message);
+                    console.log(error.message);
 
+                }
+            });
+            setUserMenuOptions(<li ><button onClick={() => setBodyContent("GalleryManager")}>Gallery</button></li>);
         }
-        };
-    }
+    }, [])
+    
 
     return(
 
@@ -53,7 +61,7 @@ function Header(){
             <ul className="navbar">
                 <li ><button onClick={() => setBodyContent("Home")}>Home</button></li>
                 <li ><button onClick={() => setBodyContent("Gallery")}>Gallery</button></li>
-                {username!="Guest" && <li ><button onClick={() => setBodyContent("GalleryManager")}>Gallery</button></li>}
+                {userMenuOptions}
                 <li ><button onClick={logInOut}>{logInOutStr}</button></li>
                 <li ><button >{username}</button></li>
             </ul>
